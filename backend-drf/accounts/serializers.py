50f46a548dd966dee4ad4476bdd4c5027cfb5c70
Rushi-04 <borkarananta028@gmail.com>
2025-06-30 17:49:48 +0530

    create user serializer

---
+++ backend-drf/accounts/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data['username'],
+            validated_data['email'],
+            validated_data['password']
+        )
+        # user = User.objects.create_user(**validated_data) #Only when we have --> fields = ['username', 'email', 'password'] i.e requierd data, not extra than this.
+        return user
