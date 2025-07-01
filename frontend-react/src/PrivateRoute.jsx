cb96b7cd86174a1edccdd7583a4629911bff9be5
Rushi-04 <borkarananta028@gmail.com>
2025-07-01 22:36:02 +0530

    Private Routes handled

--- frontend-react/src/PrivateRoute.jsx
+++ frontend-react/src/PrivateRoute.jsx
@@ -4,7 +4,11 @@ import { Navigate } from 'react-router-dom'
 
 const PrivateRoute = ({children}) => {
     const { isLoggedIn } = useContext(AuthContext);
-
+    return isLoggedIn ? (
+        children
+    ) : (
+        <Navigate to='/login' />
+    )
 }
 
 export default PrivateRoute
