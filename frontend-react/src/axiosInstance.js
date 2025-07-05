2d49ac4b261637ea4d2dd9af8364845db49251ba
Rushi-04 <borkarananta028@gmail.com>
2025-07-05 22:40:31 +0530

    axios fixed

--- frontend-react/src/axiosInstance.js
+++ frontend-react/src/axiosInstance.js
@@ -30,7 +30,7 @@ axiosInstance.interceptors.response.use(
     //Handle failed responses
     async function(error){  
         const originalRequest = error.config;
-        if(error.response.status === 401 && !originalRequest.retry){
+        if(error.response.status === 401 && !originalRequest.retry){  
             originalRequest.retry = true;
             const refreshToken = localStorage.getItem('refreshToken');
             try{  
