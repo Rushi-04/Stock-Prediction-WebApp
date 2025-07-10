7d83315d81a69c932ec9ef08b8bd5d489e2d8964
Rushi-04 <borkarananta028@gmail.com>
2025-07-11 00:39:06 +0530

    CSRF Fixed

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -15,7 +15,7 @@ from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-CSRF_TRUSTED_ORIGINS= [""]
+CSRF_TRUSTED_ORIGINS= ["https://stock-prediction-webapp-production.up.railway.app"]
 
 
 # Quick-start development settings - unsuitable for production
