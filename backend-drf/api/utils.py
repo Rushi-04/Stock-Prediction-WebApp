e4ebbb4869de4ad9c5c5ae4a0a4ae312a16db09b
Rushi-04 <borkarananta028@gmail.com>
2025-07-11 09:45:47 +0530

    fix check media folder exists

--- backend-drf/api/utils.py
+++ backend-drf/api/utils.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 
 def save_img(plot_img_path):
     image_path = os.path.join(settings.MEDIA_ROOT, plot_img_path)
+    
+    os.makedirs(os.path.dirname(image_path), exist_ok=True)
+    
     plt.savefig(image_path, dpi=300)
     plt.close()
     return settings.MEDIA_URL + plot_img_path
